5cb3c76da8262093698b71ae6dfd4da72c237a08
silen <zm7413299@163.com>
2017-02-05 23:45:39 +0800

    test

--- index.php
+++ index.php
@@ -18,3 +18,30 @@ $respons = new \Symfony\Component\HttpFoundation\Response(sprintf(
 ));
 
 $request->send();
+
+
+/*
+ * 测试http
+ * */
+// 请求的URI (e.g. /about)
+$request->getPathInfo();
+
+// 分别得到GET参数或POST参数
+$request->query->get('foo'); // GET
+$request->request->get('bar', '如何没有bar的默认值'); // POST
+
+// 得到服务器变量
+$request->server->get('HTTP_HOST');
+
+// 得到上传文件对象
+$request->files->get('foo');
+
+// 得到cookie值
+$request->cookies->get('PHPSESSID');
+
+// 得到http请求头信息
+$request->headers->get('host');
+$request->headers->get('content_type');
+
+$request->getMethod();    // GET, POST, PUT, DELETE, HEAD
+$request->getLanguages(); // 得到客户端接收语言数组
