3d251416dffbb0758eda662ec9d561de4cfe9323
zhangming <zm7413299@163.com>
2017-08-05 15:19:53 +0800

    add service

---
+++ src/base/lib/events/service.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+* 
+*/
+class base_events_service {
+
+
+    /**
+     * The event handler mappings for the application.
+     *
+     * @var array
+     */
+	
+	function __construct()
+	{
+		# code...
+	}
+
+	
+}
