8ffef2a16a9a3e7892b4c64705a406ee64201913
zhangming <zm7413299@163.com>
2017-08-11 16:20:40 +0800

    boot 启动

--- src/base/lib/events/service.php
+++ src/base/lib/events/service.php
@@ -24,5 +24,38 @@ class base_events_service {
         return $this;
     }
 
-	
+    /**
+     * 注册触发事件的listen
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //调用注册后，不需要再初始化
+        event::isInitEvents(false);
+
+        foreach ((array)$this->listen as $event => $listeners)
+        {
+            foreach( (array)$listeners as $listenerArr )
+            {
+                list( $listener, $isSync, $priority ) = $listenerArr;
+                $isSync = $isSync ? : 'sync';
+                $priority = $priority ? : 0;
+
+                $queue = ($isSync == 'async') ? $listenerArr['queue'] : null;//指定执行队列
+
+                event::listen($event, $listener, $isSync, $priority, $queue);
+            }
+        }
+    }
+
+    /**
+     * Get the events and handlers.
+     *
+     * @return array
+     */
+    public function listens()
+    {
+        return $this->listen;
+    }
 }
