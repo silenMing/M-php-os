f4431094488f71c7bc6d052f4e1e68a49a5c90a1
zhangming <zm7413299@163.com>
2017-08-25 16:29:36 +0800

    redis

--- src/base/events/quenes/redis.php
+++ src/base/events/quenes/redis.php
@@ -3,6 +3,13 @@
 class system_queues_redis implements events_interface_adapter{
 
     /**
+     * 创建执行队列的有效时间
+     *
+     * @var int|null
+     */
+    protected $expire = 3600;
+
+    /**
      * 创建一个队列任务
      *
      * @param  string  $queueName 队列标示
@@ -22,7 +29,8 @@ class system_queues_redis implements events_interface_adapter{
      * @return mixed
      */
     public function pushRaw($payload, $queueName ){
-
+        redis::scene('queue')->rpush($queueName, $payload);
+//        return Arr::get(json_decode($payload, true), 'id');
     }
 
     /**
@@ -42,7 +50,7 @@ class system_queues_redis implements events_interface_adapter{
      * @return void
      */
     public function ack($queueName, $queueData){
-
+        return redis::scene('queue')->zrem($queueName.':reserved', $queueData);
     }
 
     /**
@@ -51,11 +59,13 @@ class system_queues_redis implements events_interface_adapter{
      * @param string $queue
      */
     public function purge($queueName){
-
+        redis::scene('queue')->ltrim($queueName,-1,0);
     }
 
     public function is_end($queueName){
 
     }
+
+
 
 }
